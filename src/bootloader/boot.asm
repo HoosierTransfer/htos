4f473989d60ed88568117ceb934a3e88ddecf93a
HoosierTransfer <97118529+HoosierTransfer@users.noreply.github.com>
2023-02-24 13:53:45 +0000

    penis and balls

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -55,7 +55,7 @@ start:
     push es
     push word .after
     retf
-.after
+.after:
 
     mov [ebr_drive_number], dl
 
@@ -76,6 +76,118 @@ start:
     inc dh
     mov [bdb_heads], dh
 
+    mov ax, [bdb_sectors_per_fat]
+    mov bl, [bdb_fat_count]
+    xor ah, ah
+    mul bx
+    add ax, [bdb_reserved_sectors]
+    push ax
+
+    mov ax, [bdb_dir_entries_count]
+    shl ax, 5
+    xor dx, dx
+    div word [bdb_bytes_per_sector]
+
+    test dx, dx
+    jnz .root_dir_after
+    inc ax
+
+.root_dir_after:
+
+    mov cl, al
+    pop ax
+    mov dl, [ebr_drive_number]
+    mov bx, buffer
+    call disk_read
+
+    xor bx, bx
+    mov di, buffer
+
+.search_kernel:
+    mov si, file_kernel_bin
+    mov cx, 11
+    push di
+
+    repe cmpsb
+
+    pop di
+    je .found_kernel
+
+    add di, 32
+    inc bx
+    cmp bx, [bdb_dir_entries_count]
+    jl .search_kernel
+
+    jmp kernel_not_found_error
+
+.found_kernel:
+
+    ; di should have the address to the entry
+    mov ax, [di + 26]                   ; first logical cluster field (offset 26)
+    mov [kernel_cluster], ax
+
+    ; load FAT from disk into memory
+    mov ax, [bdb_reserved_sectors]
+    mov bx, buffer
+    mov cl, [bdb_sectors_per_fat]
+    mov dl, [ebr_drive_number]
+    call disk_read
+
+    ; read kernel and process FAT chain
+    mov bx, KERNEL_LOAD_SEGMENT
+    mov es, bx
+    mov bx, KERNEL_LOAD_OFFSET
+
+.load_kernel_loop:
+    mov ax, [kernel_cluster]
+    add ax, 31
+
+    mov cl, 1
+    mov dl, [ebr_drive_number]
+    call disk_read
+
+    add bx, [bdb_bytes_per_sector]
+
+    mov ax, [kernel_cluster]
+    mov cx, 3
+    mul cx
+    mov cx, 2
+    div cx
+
+    mov si, buffer
+    add si, ax
+    mov ax, [ds:si]
+
+    or dx, dx
+    js .even
+
+.odd:
+    shr ax, 4
+    jmp .next_cluster_after
+
+.even:
+    and ax, 0x0FFF
+
+.next_cluster_after:
+    cmp ax, 0x0FF8
+    jae .read_finish
+    
+    mov [kernel_cluster], ax
+    jmp .load_kernel_loop
+
+.read_finish:
+
+
+    mov dl, [ebr_drive_number]
+    
+    mov ax, KERNEL_LOAD_SEGMENT
+    mov ds, ax
+    mov es, ax
+
+    jmp KERNEL_LOAD_SEGMENT:KERNEL_LOAD_OFFSET
+
+    jmp wait_key_and_reboot
+
     cli
     hlt
 
@@ -84,12 +196,15 @@ floppy_error:
     call puts
     jmp wait_key_and_reboot
 
-    hlt
+kernel_not_found_error:
+    mov si, msg_kernel_not_found
+    call puts
+    jmp wait_key_and_reboot
 
 wait_key_and_reboot:
     mov ah, 0
     int 16h
-    jmp 0FFFFh
+    jmp 0FFFFh:0
 
 .halt
     cli
@@ -126,6 +241,7 @@ lba_to_chs:
     xor dx, dx
     div word [bdb_sectors_per_track]
 
+
     inc dx
     mov cx, dx
 
@@ -143,6 +259,7 @@ lba_to_chs:
     ret
 
 disk_read:
+
     push ax
     push bx
     push cx
@@ -161,9 +278,9 @@ disk_read:
     stc
     int 13h
     jnc .done
-    call disk_reset
 
     popa
+    call disk_reset
 
     dec di
     test di, di
@@ -191,8 +308,17 @@ disk_reset:
     popa
     ret
 
-msg_hello: db 'Loading...', ENDL, 0
-msg_disk_read_failed db 'Disk read failed!', ENDL, 0
+msg_hello:            db 'Loading...', ENDL, 0
+msg_disk_read_failed:        db 'Read from disk failed!', ENDL, 0
+msg_kernel_not_found:   db 'KERNEL.BIN file not found!', ENDL, 0
+file_kernel_bin:        db 'KERNEL  BIN'
+kernel_cluster:         dw 0
+
+KERNEL_LOAD_SEGMENT     equ 0x2000
+KERNEL_LOAD_OFFSET      equ 0
+
 
 times 510-($-$$) db 0
 dw 0AA55h
+
+buffer:
